35ee4f91160fb50088f579ca5802c7c41555a61d
Ben Fulcher <ben.d.fulcher@gmail.com>
2016-11-21 14:54:54 +1100

    Updates to geneEnrichmentDistance

--- Analysis/geneEnrichmentDistance.m
+++ Analysis/geneEnrichmentDistance.m
@@ -1,4 +1,4 @@
-function geneEnrichmentDistance()
+% function geneEnrichmentDistance()
 % Quantify for each gene the correlation between its coexpression and the
 % pairwise distance between regions
 %-------------------------------------------------------------------------------
@@ -8,9 +8,11 @@ function geneEnrichmentDistance()
 %-------------------------------------------------------------------------------
 % Parameters:
 %-------------------------------------------------------------------------------
-whatCorr = 'Spearman';
+whatCorr = 'Spearman'; % 'Pearson', 'Spearman'
 energyOrDensity = 'energy'; % what gene expression data to use
 pValOrStat = 'stat'; % 'pval','stat'
+normalizeHow = 'mixedSigmoid';
+thresholdGoodGene = 0.2; % threshold of valid coexpression values at which a gene is kept
 
 %-------------------------------------------------------------------------------
 % Load in and process data:
@@ -18,54 +20,52 @@ pValOrStat = 'stat'; % 'pval','stat'
 % Connectivity data:
 C = load('Mouse_Connectivity_Data.mat');
 
+%-------------------------------------------------------------------------------
 % Gene expression data:
 G = LoadMeG();
-GData = G.GeneExpData.(energyOrDensity);
+GData.raw = G.GeneExpData.(energyOrDensity);
+numGenes = size(GData.raw,2);
+% Normalize expression levels across brain regions for each gene:
+GData.z = BF_NormalizeMatrix(GData.raw,normalizeHow);
 % z-score across genes for each brain region:
-zGData = BF_NormalizeMatrix(GData','zscore')';
+GData.zz = BF_NormalizeMatrix(GData.z','zscore')';
+fprintf(1,'Gene data normalized.\n');
 
-% Pairwise euclidean distance data for control:
+%-------------------------------------------------------------------------------
+% Pairwise Euclidean distance data:
 d = C.Dist_Matrix{1,1}/1000;
+% Convert to vector of upper diagonal
 d_upper = d(triu(true(size(d)),1));
 
 %-------------------------------------------------------------------------------
-% Now we want to score genes:
+% Score genes:
 %-------------------------------------------------------------------------------
-rsfMRI_upper = rsfMRI(triu(true(size(rsfMRI)),1));
-for i = 1:G.numGenes
-    % Compute this gene's correlation pattern:
-    g = zGData(:,i);
-    % gz = (g-nanmean(g))/nanstd(g);
+fprintf(1,'Scoring %u genes on coexpression with distance\n',numGenes);
+for i = 1:numGenes
+    % This gene's correlation pattern across regions:
+    g = GData.zz(:,i);
+    % Correlation with itself (assuming normal distribution of expression):
     ggBlock = g*g';
+    % Convert to vector of upper diagonal
     ggBlock_upper = ggBlock(triu(true(size(ggBlock)),1));
 
     % Only compute correlation for gene score when the proportion of missing
     % data is under 20%:
-    if mean(isnan(ggBlock_upper)) < 0.2
-        isGood = ~isnan(ggBlock_upper);
-        [rho,pVal] = corr(ggBlock_upper(isGood),d_upper(isGood),'type',whatCorr);
-        % [rho,pVal] = corr(ggBlock_upper(isGood),rsfMRI_upper(isGood),'type',whatCorr);
+    if mean(isnan(ggBlock_upper)) < thresholdGoodGene
+        [rho,pVal] = corr(ggBlock_upper,d_upper,'rows','pairwise','type',whatCorr);
         switch pValOrStat
         case 'pVal'
             geneScores(i) = pVal;
         case 'stat'
-            geneScores(i) = abs(rho);
+            geneScores(i) = rho;
         end
     else
         geneScores(i) = NaN;
     end
 end
 
-
 %-------------------------------------------------------------------------------
-% Plot the genes with the highest scores, or a range of scores
-%-------------------------------------------------------------------------------
-% [~,ix] = sort(geneScores,'ascend');
-% f = figure('color','w');
-% plot()
-
-%-------------------------------------------------------------------------------
-% Now we can run enrichment on them:
+% Filter to genes with valid scores:
 %-------------------------------------------------------------------------------
 geneEntrez = [G.GeneStruct.gene_entrez_id];
 % Filtering:
@@ -80,8 +80,30 @@ else
 end
 
 %-------------------------------------------------------------------------------
-% Write them out to ermine J:
-writeErmineJFile(sprintf('corr_d_%s_%s_%s',whatCorr,whatGeneData,pValOrStat),...
-                geneScoresWrite,geneEntrezWrite,sprintf('%s_%s_%s',whatCorr,whatGeneData,pValOrStat));
+% Look at the distribution
+%-------------------------------------------------------------------------------
+f = figure('color','w');
+histogram(geneScoresWrite)
+xlabel('Correlation between distance and coexpression')
 
+%-------------------------------------------------------------------------------
+% Write them out to ermine J:
+%-------------------------------------------------------------------------------
+fileNameBase = sprintf('corr_d_%s_%s_%s',whatCorr,normalizeHow,pValOrStat);
+doWhat = {'pos','neg','abs'};
+for i = 1:length(doWhat)
+    fileName = sprintf('%s_%s.txt',fileNameBase,doWhat{i});
+    switch doWhat
+    case 'pos'
+        geneScoresNow = geneScoresWrite;
+    case 'neg'
+        geneScoresNow = -geneScoresWrite;
+    case 'abs'
+        geneScoresNow = abs(geneScoresWrite);
+    end
+
+    writeErmineJFile(fileName,geneScoresNow,geneEntrezWrite,...
+                    sprintf('%s_%s_%s',whatCorr,normalizeHow,pValOrStat));
 end
+
+% end
