f6ce669f6676ea2f39f265acd2209f8ae497ce63
Ben Fulcher <ben.d.fulcher@gmail.com>
2016-12-20 14:35:42 +1100

    filtering GO category sizes by matches to our data

--- DataProcessing/GetFilteredGOData.m
+++ DataProcessing/GetFilteredGOData.m
@@ -1,10 +1,13 @@
-function [GOTable,geneEntrezAnnotations] = GetFilteredGOData(whatFilter,sizeFilter)
+function [GOTable,geneEntrezAnnotations] = GetFilteredGOData(whatFilter,sizeFilter,ourEntrez)
 
 if nargin < 1
     whatFilter = 'biological_process';
 end
 if nargin < 2
     sizeFilter = [5,200];
+end
+if nargin < 3
+    ourEntrez = [];
 end
 
 % Get GO annotation data (processed):
@@ -25,7 +28,15 @@ geneEntrezAnnotations = geneEntrezAnnotations(ib);
 
 % Filter by category size:
 numGOCategories = length(allGOCategories);
-sizeGOCategories = cellfun(@length,geneEntrezAnnotations);
+if isempty(ourEntrez)
+    fprintf(1,'Filtering on actual annotated size of GO category\n');
+    sizeGOCategories = cellfun(@length,geneEntrezAnnotations);
+else
+    fprintf(1,'Filtering on size of GO category after matching to our genes\n');
+    % Make sure all annotations match those in our set of entrez_ids
+    geneEntrezAnnotations = cellfun(@(x)x(ismember(x,ourEntrez)),geneEntrezAnnotations);
+    sizeGOCategories = cellfun(@length,geneEntrezAnnotations);
+end
 isGoodSize = (sizeGOCategories >= sizeFilter(1)) & (sizeGOCategories <= sizeFilter(2));
 allGOCategories = allGOCategories(isGoodSize);
 geneEntrezAnnotations = geneEntrezAnnotations(isGoodSize);
