0dbb94210b97b1675a0f8c18f880f59b2c3a5cf4
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-06-09 13:36:00 +1000

    Shuffle nulls implemented

--- Analysis/NodeShuffleEnrichment.m
+++ Analysis/NodeShuffleEnrichment.m
@@ -2,28 +2,61 @@
 % distribution for each category separately
 numNulls = 200;
 whatEnrichment = 'degree';
+whatShuffle = 'anatomyTwo'; % 'anatomy', 'full'
+structureFilter = 'cortex';
 
 %-------------------------------------------------------------------------------
-% Get region-based anatomical nulls:
+% Load data:
 %-------------------------------------------------------------------------------
+[A_bin,regionAcronyms,adjPVals] = GiveMeAdj('Oh',0.05,true,'NCD','right');
+[geneData,geneInfo,structInfo] = LoadMeG({'none','none'},'energy');
+if strcmp(structureFilter,'cortex')
+    keepStruct = strcmp(structInfo.divisionLabel,'Isocortex');
+    geneData = geneData(keepStruct,:);
+    structInfo = structInfo(keepStruct,:);
+    A_bin = A_bin(keepStruct,keepStruct);
+end
 [GOTable,geneEntrezAnnotations] = GetFilteredGOData('biological_process',[5,100],geneInfo.entrez_id);
 sizeGOCategories = cellfun(@length,geneEntrezAnnotations);
 numGOCategories = height(GOTable);
 
+%-------------------------------------------------------------------------------
+% Define gene scoring function
+%-------------------------------------------------------------------------------
+switch whatEnrichment
+case 'degree'
+    % Score based on correlations to degree
+    k = sum(A_bin,1)' + sum(A_bin,2);
+    score_fn = @(x) corr(k,x,'type','Spearman','rows','pairwise');
+end
+
+%-------------------------------------------------------------------------------
+% Define shuffle function
+%-------------------------------------------------------------------------------
+switch whatShuffle
+case 'anatomyTwo'
+    shuffle_fn = @()AnatomyShuffle(structInfo.divisionLabel,'twoBroad');
+case 'anatomyFive'
+    shuffle_fn = @()AnatomyShuffle(structInfo.divisionLabel,'fiveByEye');
+case 'all'
+    shuffle_fn = @()randperm(size(geneData,1));
+end
+
+%-------------------------------------------------------------------------------
+% Assign scores to categories of genes
+%-------------------------------------------------------------------------------
 categoryScores = nan(numGOCategories,numNulls+1);
-k = sum(A_bin,1)' + sum(A_bin,2);
 parfor n = 1:numNulls+1
     fprintf(1,'Null %u/%u\n',n,numNulls+1);
     if n == 1
         permVector = 1:size(geneData,1);
     else
-        % permVector = randperm(size(geneData,1));
-        permVector = AnatomyShuffle(structInfo.divisionLabel,'twoBroad');
+        permVector = shuffle_fn();
     end
 
     gScore = zeros(height(geneInfo),1);
     for i = 1:height(geneInfo)
-        gScore(i) = corr(k,geneData(permVector,i),'type','Spearman','rows','pairwise');
+        gScore(i) = score_fn(geneData(permVector,i));
     end
 
     % Record mean scores for each category:
