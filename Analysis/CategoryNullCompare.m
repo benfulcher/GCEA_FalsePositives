4ec9d1e50d4b8c6aeae2a4bcf474012d317d24d3
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-09-02 21:32:54 +1000

    Tweaking detailed null investigation

--- Analysis/CategoryNullCompare.m
+++ Analysis/CategoryNullCompare.m
@@ -1,85 +1,131 @@
 %-------------------------------------------------------------------------------
 % CategoryNullCompare
+% Does nulls for correlations with degree, in different ways
 %-------------------------------------------------------------------------------
 
-whatCategory = 17;
-whatCorr = 'Pearson';
+% Settings:
+whatGOID = 6099;
+whatCorr = 'Spearman'; % 'Pearson', 'Spearman'
+numNulls = 800;
+whatShuffle = 'twoIsocortex';
 
-% [GOTable,geneEntrezAnnotations] = GetFilteredGOData('biological_process',[5,100],geneInfo.entrez_id);
+%-------------------------------------------------------------------------------
+% Load defaults:
+cParam = GiveMeDefaultParams('conn');
+gParam = GiveMeDefaultParams('gene');
+eParam = GiveMeDefaultParams('enrichment');
+
+% Get gene data:
+[geneData,geneInfo,structInfo] = LoadMeG(gParam);
+
+% Load in GO annotations:
+[GOTable,geneEntrezAnnotations] = GetFilteredGOData(eParam.processFilter,eParam.sizeFilter,...
+                                    geneInfo.entrez_id);
 sizeGOCategories = cellfun(@length,geneEntrezAnnotations);
 numGOCategories = height(GOTable);
+whatCategory = find(GOTable.GOID==whatGOID);
+fprintf(1,'Looking in at %s:%s (%u)\n',GOTable.GOIDlabel{whatCategory},...
+                    GOTable.GOName{whatCategory},GOTable.size(whatCategory));
+
+% Load adjacency matrix:
+[A_bin,regionAcronyms,adjPVals] = GiveMeAdj(cParam.connectomeSource,cParam.pThreshold,true,...
+                                    cParam.whatWeightMeasure,cParam.whatHemispheres,cParam.structFilter);
 k = sum(A_bin,1)' + sum(A_bin,2);
 
 theGenesEntrez = geneEntrezAnnotations{whatCategory};
 matchMe = find(ismember(geneInfo.entrez_id,theGenesEntrez));
 
-numNulls = 500;
 categoryScores = nan(numNulls+1,3);
 for n = 1:numNulls+1
-    fprintf(1,'Null %u/%u\n',n,numNulls+1);
 
     if n == 1
-        permVector = 1:size(geneData,1);
+        fprintf(1,'True ordering!');
+        permVectorAll = 1:size(geneData,1);
+        permVectorCustom = 1:size(geneData,1);
     else
-        % permVector = randperm(size(geneData,1));
-        permVector = AnatomyShuffle(structInfo.divisionLabel,'twoBroad');
+        fprintf(1,'Null %u/%u\n',n+1,numNulls+1);
+        permVectorAll = randperm(size(geneData,1));
+        permVectorCustom = AnatomyShuffle(structInfo.divisionLabel,whatShuffle);
     end
 
     %-------------------------------------------------------------------------------
     % Shuffling node properties:
     %-------------------------------------------------------------------------------
-    gScore = zeros(length(theGenesEntrez),1);
+    gScore = zeros(length(theGenesEntrez),2);
     for i = 1:length(theGenesEntrez)
-        gScore(i) = corr(k,geneData(permVector,matchMe(i)),'type',whatCorr,'rows','pairwise');
+        gScore(i,1) = corr(k,geneData(permVectorAll,matchMe(i)),'type',whatCorr,'rows','pairwise');
+        gScore(i,2) = corr(k,geneData(permVectorCustom,matchMe(i)),'type',whatCorr,'rows','pairwise');
     end
 
     % Record mean scores for each category:
-    categoryScores(n,1) = nanmean(gScore);
+    categoryScores(n,1) = nanmean(gScore(:,1));
+    categoryScores(n,2) = nanmean(gScore(:,2));
 
     %-------------------------------------------------------------------------------
     % Taking genes at random:
     %-------------------------------------------------------------------------------
-    rp = randperm(size(geneData,2),length(theGenesEntrez));
-    gScore = zeros(length(theGenesEntrez),1);
-    for i = 1:length(theGenesEntrez)
-        gScore(i) = corr(k,geneData(:,rp(i)),'type',whatCorr,'rows','pairwise');
+    if n == 1
+        categoryScores(1,3) = categoryScores(1,1);
+    else
+        rp = randperm(size(geneData,2),length(theGenesEntrez));
+        gScore = zeros(length(theGenesEntrez),1);
+        for i = 1:length(theGenesEntrez)
+            gScore(i) = corr(k,geneData(:,rp(i)),'type',whatCorr,'rows','pairwise');
+        end
+        categoryScores(n,3) = nanmean(gScore);
     end
-    categoryScores(n,2) = nanmean(gScore);
 
     %-------------------------------------------------------------------------------
     % Both randomized!:
     %-------------------------------------------------------------------------------
-    rp = randperm(size(geneData,2),length(theGenesEntrez));
-    gScore = zeros(length(theGenesEntrez),1);
-    for i = 1:length(theGenesEntrez)
-        gScore(i) = corr(k,geneData(permVector,rp(i)),'type',whatCorr,'rows','pairwise');
-    end
-    categoryScores(n,3) = nanmean(gScore);
-
+    % rp = randperm(size(geneData,2),length(theGenesEntrez));
+    % gScore = zeros(length(theGenesEntrez),1);
+    % for i = 1:length(theGenesEntrez)
+    %     gScore(i) = corr(k,geneData(permVector,rp(i)),'type',whatCorr,'rows','pairwise');
+    % end
+    % categoryScores(n,3) = nanmean(gScore);
 end
 
 %-------------------------------------------------------------------------------
+% Estimate p-values
+%-------------------------------------------------------------------------------
+nullInd = 2:numNulls+1;
+pValZ = arrayfun(@(x)1-normcdf(categoryScores(1,x),mean(categoryScores(nullInd,x)),...
+                    std(categoryScores(nullInd,x))),1:3);
+labels = {'shuffle-all',whatShuffle,'random-gene'};
+for i = 1:3
+    fprintf(1,'(%s): p_Z = %g\n',labels{i},pValZ(i));
+end
+
+%-------------------------------------------------------------------------------
+% Plot null distributions:
+%-------------------------------------------------------------------------------
 f = figure('color','w'); ax = gca; hold on
-plot(ones(2,1)*mean(categoryScores(2:end,1)),ax.YLim,'color',h1.FaceColor);
-plot(ones(2,1)*mean(categoryScores(:,3)),ax.YLim,'color',h3.FaceColor);
+% First the three nulls as histograms:
 h1 = histogram(categoryScores(2:end,1)); h1.FaceColor = [0.2157    0.4941    0.7216];
-h3 = histogram(categoryScores(:,3)); h3.FaceColor = [0.5961    0.3059    0.6392];
-h2 = histogram(categoryScores(:,2)); h2.FaceColor = [0.3020    0.6863    0.2902];
-plot(ones(2,1)*mean(categoryScores(:,2)),ax.YLim,'color',h2.FaceColor);
+h2 = histogram(categoryScores(2:end,2)); h2.FaceColor = [0.5961    0.3059    0.6392];
+h3 = histogram(categoryScores(2:end,3)); h3.FaceColor = [0.3020    0.6863    0.2902];
+plot(ones(2,1)*mean(categoryScores(2:end,1)),ax.YLim,'color',h1.FaceColor);
+plot(ones(2,1)*mean(categoryScores(2:end,2)),ax.YLim,'color',h2.FaceColor);
+plot(ones(2,1)*mean(categoryScores(2:end,3)),ax.YLim,'color',h3.FaceColor);
 plot(ones(2,1)*categoryScores(1,1),ax.YLim,'r')
-title(sprintf('%s\t%s (%u)',GOTable.GOIDlabel{whatCategory},GOTable.GOName{whatCategory},GOTable.size(whatCategory)))
+title(sprintf('%s\t%s (%u)',GOTable.GOIDlabel{whatCategory},...
+                    GOTable.GOName{whatCategory},GOTable.size(whatCategory)))
 xlabel(sprintf('%s correlation',whatCorr))
-legend([h1,h2,h3],{'random-degree','random-gene','random-both'})
+legend([h1,h2,h3],{'random-area-all',sprintf('random-area-%s',whatShuffle),'random-gene'})
 
 %-------------------------------------------------------------------------------
 f = figure('color','w'); ax = gca;
-corrMat = corr(geneData(:,matchMe),'type','Pearson','rows','pairwise');
-ord = BF_ClusterReorder(corrMat,'euclidean','average')
+corrMat = corr(geneData(:,matchMe),'type',whatCorr,'rows','pairwise');
+ord = BF_ClusterReorder(corrMat,'euclidean','average');
 imagesc(corrMat(ord,ord));
 ax.YTick = 1:length(matchMe);
 acronymsMatch = geneInfo.acronym(matchMe);
 ax.YTickLabel = acronymsMatch(ord);
 caxis([-1,1])
 colormap([flipud(BF_getcmap('blues',9));1,1,1;BF_getcmap('reds',9)])
-title(sprintf('%s\t%s (%u)',GOTable.GOIDlabel{whatCategory},GOTable.GOName{whatCategory},GOTable.size(whatCategory)))
+title(sprintf('%s %s (%u genes)[%s]',GOTable.GOIDlabel{whatCategory},...
+                    GOTable.GOName{whatCategory},GOTable.size(whatCategory),...
+                    whatCorr))
 axis('square')
+cB = colorbar;
