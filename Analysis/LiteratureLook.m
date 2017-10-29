9f542146d3b864b277362108533b2e0990052b9a
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-10-29 18:27:26 +1100

    Visualizing literature enrichment results

--- Analysis/LiteratureLook.m
+++ Analysis/LiteratureLook.m
@@ -1,7 +1,8 @@
 % function LiteratureLook(whatSpecies)
 
 % if nargin < 1
-    whatSpecies = 'mouse';
+    whatSpecies = 'human';
+    theThreshold = 0.2;
 % end
 
 % Load in data:
@@ -26,6 +27,12 @@ allGOIDs = sort(unique(vertcat(allGOIDs{:})),'ascend');
 numGOIDs = length(allGOIDs);
 fprintf(1,'Annotations span %u GO categories\n',length(allGOIDs));
 
+% Match to names
+eParam = GiveMeDefaultParams('enrichment');
+eParam.sizeFilter = [0,1e6];
+[GOTerms,geneEntrezAnnotations] = GetFilteredGOData(eParam.whatSource,...
+                    eParam.processFilter,eParam.sizeFilter);
+
 % Convert each dataset to a vector across GOIDs:
 rowVectorResults = nan(numSpecies,numGOIDs);
 for i = 1:numSpecies
@@ -34,35 +41,50 @@ for i = 1:numSpecies
 end
 
 % Filter max:
-theThreshold = 0.1;
 underThreshold = @(x) 1-(1-x)*double(x < theThreshold); % results over threshold are set to 1
 rowVectorResultsTh = arrayfun(underThreshold,rowVectorResults);
-rowVectorResultsTh(rowVectorResultsTh==1) = NaN;
+rowVectorResultsTh(isnan(rowVectorResultsTh)) = 1;
 
 % Sort:
-meansGO = nanmean(~isnan(rowVectorResultsTh),1);
-meansDatasets = nanmean(~isnan(rowVectorResultsTh),2);
+meansGO = nanmean(rowVectorResultsTh,1);
+meansDatasets = nanmean(rowVectorResultsTh,2);
 
-[~,ix] = sort(meansGO,'descend');
+[~,ix] = sort(meansGO,'ascend');
 allGOIDsSort = allGOIDs(ix);
-[~,iy] = sort(meansDatasets,'descend');
+[~,iy] = sort(meansDatasets,'ascend');
 speciesTableNamesSort = speciesTableNames(iy);
 
 rowVectorResultsThSort = rowVectorResultsTh(iy,ix);
 
 % Trim:
-hasNoAnnotations = (meansGO(ix)==0);
+numTrimmed = sum(~hasNoAnnotations);
+hasNoAnnotations = (meansGO(ix)==1);
 allGOIDsSort(hasNoAnnotations) = [];
 rowVectorResultsThSort(:,hasNoAnnotations) = [];
 
+% IDs -> GONames
+GONamesSort = cell(numTrimmed,1);
+for i = 1:numTrimmed
+    weHere = (GOTerms.GOID==allGOIDsSort(i));
+    if ~any(weHere)
+        GONamesSort{i} = '<unknown>';
+    else
+        GONamesSort{i} = sprintf('%s(%u)',GOTerms.GOName{weHere},GOTerms.size(weHere));
+    end
+end
+
 % Plot:
 f = figure('color','w'); ax = gca;
-BF_imagesc(rowVectorResultsThSort);
+imagesc(rowVectorResultsThSort);
 ax.YTick = 1:numSpecies;
 ax.YTickLabel = flipud(speciesTableNamesSort);
+ax.TickLabelInterpreter = 'none';
 ax.XTick = 1:sum(~hasNoAnnotations);
-ax.XTickLabel = allGOIDsSort;
+% ax.XTickLabel = allGOIDsSort;
+ax.XTickLabel = GONamesSort;
+ax.XTickLabelRotation = 90;
 colormap([flipud(BF_getcmap('blues',9))])
+caxis([0,theThreshold*1.2]);
 title(whatSpecies);
 
 % end
