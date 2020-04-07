1acafda1ce1208b3c9460021b51b3ed18ce9f668
Ben Fulcher <ben.d.fulcher@gmail.com>
2020-04-07 17:48:12 +1000

    Getting CFPR-literature figure for mouse-human together

--- Analysis/propLitCFPR.m
+++ Analysis/propLitCFPR.m
@@ -2,16 +2,30 @@
 
 % Parameters:
 equiprobableBins = true;
+plotInOne = true;
 
 % Plot:
 f = figure('color','w');
-subplot(1,2,1);
-PropLitFPSR('mouse','all',equiprobableBins,false)
-subplot(1,2,2);
-PropLitFPSR('human','cortex',equiprobableBins,false)
-f.Position = [744   871   533   179];
+if plotInOne
+    hold('on')
+    PropLitFPSR('mouse','all',equiprobableBins,false)
+    PropLitFPSR('human','cortex',equiprobableBins,false)
+    ax = gca();
+    ax.XScale = 'log';
+    f.Position = [744   871   300   216];
+else
+    subplot(1,2,1);
+    PropLitFPSR('mouse','all',equiprobableBins,false)
+    subplot(1,2,2);
+    PropLitFPSR('human','cortex',equiprobableBins,false)
+    f.Position = [744   871   533   179];
+end
 
 % Save:
-fileName = fullfile('OutputPlots','CFPR_Lit_Together.svg');
+if plotInOne
+    fileName = fullfile('OutputPlots','CFPR_Lit_Together_InOne.svg');
+else
+    fileName = fullfile('OutputPlots','CFPR_Lit_Together.svg');
+end
 saveas(f,fileName,'svg')
 fprintf(1,'Saved to %s\n',fileName);
