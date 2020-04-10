18e8d2750d60fd9fc38f5f8acfdb2df9fdd108be
Ben Fulcher <ben.d.fulcher@gmail.com>
2020-04-10 12:28:24 +1000

    Fixing for physics cluster

---
+++ BatchComputing/Physics/pbs_mouseCategoryNullCompute_SBP-rand_cortex.sh
@@ -0,0 +1,27 @@
+#!/bin/csh
+#PBS -N mouseCortexNullComputeSBPrand
+#PBS -o mouseCortexNull_SBPrand.txt
+#PBS -q yossarian
+#PBS -l nodes=1:ppn=12
+#PBS -l mem=144GB
+# Minimum acceptable walltime: day-hours:minutes:seconds
+#PBS -l walltime=140:00:00
+# Email user if job ends or aborts
+#PBS -m ea
+#PBS -M ben.fulcher@sydney.edu.au
+#PBS -j oe
+#PBS -V
+
+# Show the host on which the job ran
+hostname
+module load Matlab2018a
+
+# Move
+cd $PBS_O_WORKDIR
+cd ../../
+
+# Launch the Matlab job
+matlab -nodesktop -r "startup; parpool('local',12);\
+params = GiveMeDefaultParams('mouse','cortex');\
+params.e.whatEnsemble = 'randomMap';\
+NullComputation(params); exit"
---
+++ BatchComputing/Physics/pbs_mouseCategoryNullCompute_SBP-spatial_wholeBrain.sh
@@ -0,0 +1,27 @@
+#!/bin/csh
+#PBS -N mouseBrainNullComputeSBPspatial
+#PBS -o mouseBrainNull_SBPspatial.txt
+#PBS -q yossarian
+#PBS -l nodes=1:ppn=12
+#PBS -l mem=144GB
+# Minimum acceptable walltime: day-hours:minutes:seconds
+#PBS -l walltime=140:00:00
+# Email user if job ends or aborts
+#PBS -m ea
+#PBS -M ben.fulcher@sydney.edu.au
+#PBS -j oe
+#PBS -V
+
+# Show the host on which the job ran
+hostname
+module load Matlab2018a
+
+# Move
+cd $PBS_O_WORKDIR
+cd ../../
+
+# Launch the Matlab job:
+matlab -nodesktop -r "startup; parpool('local',10);\
+params = GiveMeDefaultParams('mouse','all');\
+params.e.whatEnsemble = 'customEnsemble';\
+NullComputation(params); exit"
